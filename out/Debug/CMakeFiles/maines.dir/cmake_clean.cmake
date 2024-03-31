732e4b21ddfcdf9fb6a7aef56b433f64289df1ad
rogerdurdn <roger23vel@gmail.com>
2024-03-31 01:02:08 -0600

    base config

---
+++ out/Debug/CMakeFiles/maines.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/maines.dir/main.cpp.o"
+  "CMakeFiles/maines.dir/main.cpp.o.d"
+  "maines"
+  "maines.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/maines.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
